a3bf85a0c141177d7fbbf6d23091df26167fdb94
foticc <foticc@163.com>
2025-12-08 18:02:40 +0800

    128

--- classes/globals/save_game_manager.gd
+++ classes/globals/save_game_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const save_game_data_path:String = "user://game_data/save_MainScene_game_data.tres"
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("save"):
 		save_game()
@@ -13,3 +15,6 @@ func load_geme()->void:
 	var save_level_data_component:SaveLevelDataComponent = get_tree().get_first_node_in_group("save_level_data_component")
 	if save_level_data_component!=null:
 		save_level_data_component.load_game()
+
+func has_archive()->bool:
+	return FileAccess.file_exists(save_game_data_path)
--- tscn/ui/game_menu_scene.gd
+++ tscn/ui/game_menu_scene.gd
@@ -1,9 +1,16 @@
 extends CanvasLayer
 
 @onready var save_btn: Button = $MarginContainer/VBoxContainer/SaveBtn
+@onready var is_load:bool = SaveGameManager.has_archive()
+
 
 func _ready() -> void:
 	save_btn.pressed.connect(_on_save_btn)
+	save_btn.text = "LOAD" if is_load else "SAVE"
+		
 
 func _on_save_btn()->void:
-	SaveGameManager.save_game()
+	if is_load:
+		SaveGameManager.load_geme()
+	else:
+		SaveGameManager.save_game()
